b341dd8b291cefdcca4f68ba238c84788c4739d4
AlolikaGon <jhelum95@gmail.com>
2021-11-28 20:11:19 -0500

    add reinforce for gridworld and cartpole

---
+++ cartpole_reinforce.py
@@ -0,0 +1,219 @@
+import numpy as np
+np.set_printoptions(precision=4, suppress=True)
+import matplotlib.pyplot as plt
+# import random
+import sys
+""" 
+Observation:
+    Type: Box(4)
+    Num     Observation               Min                     Max
+    0       Cart Position             -4.8                    4.8
+    1       Cart Velocity             -Inf                    Inf
+    2       Pole Angle                -0.418 rad (-24 deg)    0.418 rad (24 deg)
+    3       Pole Angular Velocity     -Inf                    Inf
+Actions:
+    Type: Discrete(2)
+    Num   Action
+    0     Push cart to the left
+    1     Push cart to the right
+    Note: The amount the velocity that is reduced or increased is not
+    fixed; it depends on the angle the pole is pointing. This is because
+    the center of gravity of the pole increases the amount of energy needed
+    to move the cart underneath it
+Reward:
+    Reward is 1 for every step taken, including the termination step
+Starting State:
+    All observations are assigned a uniform random value in [-0.05..0.05]
+Episode Termination:
+    Pole Angle is more than 12 degrees.
+    Cart Position is more than 2.4 (center of the cart reaches the edge of
+    the display).
+    Episode length is greater than 200.
+    Solved Requirements:
+    Considered solved when the average return is greater than or equal to
+    195.0 over 100 consecutive trials.
+"""
+
+X_range = [-4.8, 4.8]
+v_range = [-10, 10]#[-100000, 100000] #[float('-inf'), float('inf')]
+theta_range = [-24, 24]
+anglev_range = [-10, 10] #[-100000, 100000]#[float('-inf'), float('inf')]
+start_range = [-0.05, 0.05]
+
+terminating_cond =[2.4, 12, 200]
+
+action_set = [0,1] #left, right
+
+M = 3 # dimensionality of the fourier transform
+softmax_sigma = 0.1
+# gamma = 1
+
+def in_radian(ang):
+    return ang*np.pi/180
+
+def transition(action, x, x_dot, theta, theta_dot):
+    gravity = 9.8
+    masscart = 1.0
+    masspole = 0.1
+    total_mass = masspole + masscart
+    length = 0.5  # actually half the pole's length
+    polemass_length = masspole * length
+    force_mag = 10.0
+    tau = 0.02
+
+    force = force_mag if action == 1 else -force_mag
+    costheta = np.cos(theta) # theta in radians
+    sintheta = np.sin(theta)
+
+    # from gym https://github.com/openai/gym/blob/master/gym/envs/classic_control/cartpole.py
+    temp = (force + polemass_length * theta_dot ** 2 * sintheta) / total_mass
+    thetaacc = (gravity * sintheta - costheta * temp) / (length * (4.0 / 3.0 - masspole * costheta ** 2 / total_mass))
+    xacc = temp - polemass_length * thetaacc * costheta / total_mass
+
+    x = x + tau * x_dot
+    x_dot = x_dot + tau * xacc
+    theta = theta + tau * theta_dot
+    theta_dot = theta_dot + tau * thetaacc
+    return x, x_dot, theta, theta_dot
+    
+def is_terminating(x, x_dot, theta, theta_dot, step):
+    if x < -terminating_cond[0] or x > terminating_cond[0] or theta < -in_radian(terminating_cond[1]) or theta > in_radian(terminating_cond[1]) or step>terminating_cond[2]:
+        return True
+    return False
+
+def reward(x, x_dot, theta, theta_dot, step):
+    if is_terminating(x, x_dot, theta, theta_dot, step):
+        return 0
+    return 1
+
+def normalize(x, x_dot, theta, theta_dot, cosineflag=True):
+    if cosineflag:
+        x = (x-X_range[0])/(X_range[1]-X_range[0])
+        theta = (theta-theta_range[0])/(theta_range[1]-theta_range[0])
+        x_dot = (x_dot - v_range[0])/(v_range[1] - v_range[0])
+        theta_dot = (theta_dot - anglev_range[0])/(anglev_range[1] - anglev_range[0])
+        
+    else:
+        x = 2*(x-X_range[0])/(X_range[1]-X_range[0]) -1
+        theta = 2*(theta-theta_range[0])/(theta_range[1]-theta_range[0]) -1
+        x_dot = 2*(x_dot - v_range[0])/(v_range[1] - v_range[0]) -1
+        theta_dot = 2*(theta_dot - anglev_range[0])/(anglev_range[1] - anglev_range[0]) -1
+
+    return  x, x_dot, theta, theta_dot
+
+def fourier(x, x_dot, theta, theta_dot, cosineflag=True):
+    #normalize
+    x, x_dot, theta, theta_dot = normalize(x, x_dot, theta, theta_dot, cosineflag)
+    phi = [1]
+    if cosineflag:
+        for i in range(1, M+1):
+            phi.append(np.cos(i*np.pi*x))
+        for i in range(1, M+1):
+            phi.append(np.cos(i*np.pi*x_dot))
+        for i in range(1, M+1):
+            phi.append(np.cos(i*np.pi*theta))
+        for i in range(1, M+1):
+            phi.append(np.cos(i*np.pi*theta_dot))
+    else:
+        for i in range(1, M+1):
+            phi.append(np.sin(i*np.pi*x))
+        for i in range(1, M+1):
+            phi.append(np.sin(i*np.pi*x_dot))
+        for i in range(1, M+1):
+            phi.append(np.sin(i*np.pi*theta))
+        for i in range(1, M+1):
+            phi.append(np.sin(i*np.pi*theta_dot))
+    return np.array(phi)
+
+def softmax_action(policy_params, x, x_dot, theta, theta_dot):
+    
+    phi_s = fourier(x, x_dot, theta, theta_dot) # (4M+1, )
+    # print(policy_params.shape, phi_s.shape)
+    policy_val = np.dot(phi_s.T, policy_params) #(4M,1) (4M+1, 2)
+    policy_exp = np.exp(softmax_sigma*policy_val)
+    policy_exp /= np.sum(policy_exp)
+    # print(policy_exp, x, x_dot, theta, theta_dot)
+    return policy_exp #(2, )
+
+
+def REINFORCE(alpha_w, alpha_theta, algo_type='without_baseline', gamma=0.99):
+    policy_params = np.ones((4*M+1,len(action_set)))*0.01
+    value_params = np.ones(4*M+1)*0.01
+    episode_length = []
+
+    for iter in range(5000):
+        policy_params_temp = policy_params.copy()
+        #run episode
+        #initial state
+        x = np.random.uniform(start_range[0], start_range[1])
+        theta = np.random.uniform(start_range[0], start_range[1])
+        x_dot  = np.random.uniform(start_range[0], start_range[1])
+        theta_dot = np.random.uniform(start_range[0], start_range[1])
+        step = 1
+        state_list, action_list, reward_list = [], [], []
+        while not is_terminating(x, x_dot, theta, theta_dot, step):
+            #choose action
+            curr_action = np.argmax(softmax_action(policy_params, x, x_dot, theta, theta_dot))
+            #next state
+            next_x, next_x_dot, next_theta, mext_theta_dot = transition(curr_action, x, x_dot, theta, theta_dot)
+            #reward
+            curr_reward = reward(next_x, next_x_dot, next_theta, mext_theta_dot, step)
+            step += 1
+
+            state_list.append([x, x_dot, theta, theta_dot])
+            action_list.append(curr_action)
+            reward_list.append(curr_reward)
+            x, x_dot, theta, theta_dot = next_x, next_x_dot, next_theta, mext_theta_dot
+        print(np.array(state_list))
+        print(action_list)
+        print("\n EPISODE LENGTH: ",len(reward_list), "CURR ITER: ", iter)
+        
+        return_list = np.zeros(len(reward_list))
+        return_list[-1] = reward_list[-1]
+        T = len(reward_list)
+        for i in range(T-2, -1, -1):
+            return_list[i] = reward_list[i] + gamma*return_list[i+1]
+        episode_length.append(return_list[0])
+
+        #loop through episode
+        for t in range(T):
+            x, x_dot, theta, theta_dot = state_list[t]
+            curr_action = action_list[t]
+            phi_s = fourier(x, x_dot, theta, theta_dot)
+
+            if algo_type =='without_baseline':
+                delta = return_list[t]
+            elif algo_type == 'with_baseline':
+                val_w_s = np.dot(value_params,phi_s)
+                delta = return_list[t] - val_w_s
+            
+            policy = softmax_action(policy_params, x, x_dot, theta, theta_dot)
+            if curr_action == 0:
+                policy_params[:,0] += alpha_theta*delta*(1-policy[0])*phi_s
+                policy_params[:,1] += alpha_theta*delta*(-1*policy[0])*phi_s
+                # print(curr_action, delta, policy)
+            if curr_action == 1:
+                policy_params[:,0] += alpha_theta*delta*(-policy[1])*phi_s
+                policy_params[:,1] += alpha_theta*delta*(1-policy[1])*phi_s
+                # print(curr_action, delta, policy)
+
+            if algo_type == 'with_baseline':
+                value_params += alpha_w*delta*phi_s
+                    
+        if np.mean(episode_length[max(0, iter-100): iter+1]) > 195.0:
+            print("Hooray... solved")
+            break
+        max_diff = np.max(np.abs(policy_params_temp - policy_params))
+        print(" Max diff: ",max_diff)
+        if max_diff/alpha_theta < 0.1: # 0.001 works with 1e-6 policy_step
+            break
+
+    plt.figure()
+    plt.plot(np.arange(len(episode_length)), episode_length)
+    plt.xlabel('Iterations')
+    plt.ylabel('Avg. return')
+    # plt.ylim([-100, 1000])
+    plt.savefig('graph_cartpole_reinforce_'+str(algo_type))
+
+alpha_w, alpha_theta = 1e-7, 1e-8
+REINFORCE(alpha_w, alpha_theta, 'with_baseline')
---
+++ gridworld_reinforce.py
@@ -0,0 +1,182 @@
+import numpy as np
+np.set_printoptions(precision=4, suppress=True)
+import matplotlib.pyplot as plt
+import random
+import sys
+np.random.seed(1)
+max_r, max_c = 4, 4
+# state_value = np.zeros((5,5))
+action_set = {'AU': 0, 'AR': 1, 'AD': 2, 'AL': 3}
+action_coord = [[-1,0], [0,1], [1,0], [0,-1]]
+terminal_states = [[4,4]]
+obstacles = [[2,2], [3,2]]
+obstacle_index = [(max_r+1)*r+c for r, c in obstacles]
+water = [[4,2]]
+initial_states = [[0,0]]# [[i,j] for j in range(max_c+1) for i in range(max_r+1) if [i,j] not in obstacles+terminal_states]
+softmax_sigma=0.1
+
+print_arrow = {'AU': u'\u2191', 'AR': u'\u2192', 'AL': u'\u2190', 'AD': u'\u2193', 'G': 'G'}
+
+def transition_function(state_r, state_c, action, prob = [0.8, 0.05, 0.05, 0.1]):
+# def transition_function(state_r, state_c, action, prob = [1.0, 0.0, 0.0, 0.0]): #deterministic
+    if action not in action_set:
+        print("wrong action")
+        sys.exit(0)
+    if state_r == max_r and state_c == max_c:
+        return []
+    action_idx = action_set[action]
+    action_idx_r = (action_idx+1)%4
+    action_idx_l = (action_idx-1)%4
+    # del_r, del_c = random.choices([action_coord[action_idx], action_coord[action_idx_r], action_coord[action_idx_l], [0,0]], weights=[0.8, 0.05, 0.05, 0.1])
+    future_states = []
+    temp = [action_coord[action_idx], action_coord[action_idx_r], action_coord[action_idx_l], [0,0]]
+    for i in range(4):
+        del_r, del_c = temp[i]
+        new_state_r, new_state_c = state_r, state_c
+        if 0<=state_r + del_r <= max_r:
+            new_state_r += del_r
+        if 0<=state_c + del_c <= max_c:
+            new_state_c += del_c
+        if [new_state_r, new_state_c] in obstacles:
+            new_state_r, new_state_c = state_r, state_c
+        future_states.append([new_state_r, new_state_c, prob[i]]) 
+    return future_states
+
+def reward(next_state, gold_state_reward=5):
+    if next_state in terminal_states:
+        return 10
+    if next_state in water:
+        return -10    
+    return 0
+
+def softmax_select(theta, phi, state_idx):
+    phi_s = phi[state_idx].T
+    # print(phi_s.shape, theta)
+    action_values = softmax_sigma*np.dot(phi_s, theta)
+    policy = np.exp(action_values)
+    policy /= sum(policy)
+    return policy
+    
+
+def runEpisode(theta, phi):
+    curr_state = random.choices(initial_states)[0]
+    state_list, action_list, reward_list = [], [], []
+    timestep = 0
+    while curr_state not in terminal_states:# and timestep < 10000:
+        state_idx = (max_r+1)*curr_state[0] + curr_state[1]
+        action_idx = np.argmax(softmax_select(theta, phi, state_idx))
+        curr_action = list(action_set.keys())[action_idx]
+        
+        possible_next_states = transition_function(curr_state[0], curr_state[1], curr_action)
+        next_state = random.choices([[s[0], s[1]] for s in possible_next_states], [s[2] for s in possible_next_states])[0]
+        curr_reward = reward(next_state)
+        # print(curr_state, curr_action, curr_reward, next_state)
+        state_list.append(curr_state)
+        action_list.append(curr_action)
+        #append discounted reward
+        reward_list.append(curr_reward)
+        curr_state = next_state
+        timestep += 1
+
+    return state_list, action_list, reward_list
+
+# for r in range(max_r+1):
+#     for c in range(max_c+1):
+#         index = (max_r+1)*r+c
+
+def REINFORCE(gamma, alpha_w, alpha_theta, algo_type = 'without_baseline'):
+    total_states = (max_r+1)*(max_c+1)# - len(obstacles)
+    theta = np.random.normal(0, 0.1, (total_states, len(action_set))) #np.ones((total_states, len(action_set)))*0.01 #
+    # momentum, vel = np.zeros(theta.shape), np.zeros(theta.shape)
+    # beta1, beta2 = 0.9, 0.999
+    val_w = np.zeros((total_states))
+    phi = np.zeros((total_states, total_states))
+    np.fill_diagonal(phi, 1.0)
+    epsiodes_graph = []
+    threshold = 0.9
+    
+    for iter in range(2000):
+        state_list, action_list, reward_list = runEpisode(theta, phi)
+        T = len(reward_list)
+        # if iter%100==0:
+        print("\n EPISODE LENGTH: ",len(reward_list), "CURR ITER: ", iter)
+        return_list = np.zeros(T)
+        return_list[-1] = reward_list[-1]
+        for t in range(T-2, -1, -1):
+            return_list[t] = reward_list[t] + gamma*return_list[t+1]
+        epsiodes_graph.append(T)
+        T_range = np.arange(T)
+        # np.random.shuffle(T_range)
+        theta_temp = theta.copy()
+        for t in T_range:
+            
+            curr_state, curr_action = state_list[t], action_list[t]
+            state_idx = (max_r+1)*curr_state[0] + curr_state[1]
+            action_idx = action_set[curr_action]
+            phi_s = phi[state_idx]
+            if algo_type == 'without_baseline':
+                delta = return_list[t]
+            elif algo_type == 'with_baseline':
+                delta = return_list[t] - np.dot(phi_s, val_w)
+            #w update
+            #theta update
+            
+            policy = softmax_select(theta, phi, state_idx)
+            for i in range(len(action_set)):
+                if action_idx == i:
+                    theta[:, i] += alpha_theta*delta*(1-policy[action_idx])*phi_s
+                else:
+                    theta[:, i] += alpha_theta*delta*(-1*policy[action_idx])*phi_s
+            # print(action_idx, delta, policy)
+            
+            if algo_type == 'with_baseline':
+                val_w += alpha_w*delta*phi_s
+            #adam update
+            # momentum = beta1*momentum + (1-beta1)*delta*delta_ln_pi
+            # vel = beta2*vel + (1-beta2)*(delta*delta_ln_pi)**2
+            # delta_theta = alpha_theta*momentum/(1-beta1)/(np.sqrt(vel/(1 - beta2)) + 10**(-8))
+
+        #relative change in theta
+        max_diff = np.max(np.abs(theta_temp - theta))
+        print("max_diff ", max_diff)
+        if  max_diff/alpha_theta < threshold:
+            break
+
+
+            
+    #final policy
+    greedy_policy = [[None for i in range(max_c+1)] for j in range(max_r+1)]
+    for i in range(max_r+1):
+        for j in range(max_c+1):
+            if [i,j] in terminal_states:
+                greedy_policy[i][j] = 'G'
+                continue
+            if [i,j] in obstacles:
+                continue
+            state_idx = (max_r+1)*i + j
+            policy = softmax_select(theta, phi, state_idx)
+            action_idx = np.argmax(policy)
+            greedy_policy[i][j] = list(action_set.keys())[action_idx]
+
+    # print(greedy_policy)
+    print("final policy ")
+    for r in greedy_policy:
+        s = ''
+        for dir in r:
+            if dir in print_arrow:
+                s += print_arrow[dir] +'  '
+            else:
+                s += '   '
+        print(s)
+
+    plt.figure()
+    plt.plot(np.arange(len(epsiodes_graph)), epsiodes_graph)
+    plt.xlabel('Iterations')
+    plt.ylabel('#Episodes')
+    # plt.ylim([-100, 1000])
+    plt.savefig('graph_gridworld_reinforce_'+str(algo_type))
+
+
+alpha_w, alpha_theta = 0.0001, 1e-8
+REINFORCE(0.9, alpha_w, alpha_theta, 'without_baseline')
+
